cab713a4c2854e0bc8a9b869c88d7584c2d11400
Elmousski <a.moussier@hotmail.fr>
2018-09-27 09:58:00 +0200

    Un peu de mise en page et script pop up

--- scripts/mamain.js
+++ scripts/mamain.js
@@ -1,6 +1,20 @@
-window.setTimeout(myFunction, 10000);
+window.setTimeout(myFunction, 4000);
 
 function myFunction() {
- alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp !!!');
  
+window.confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp !!!")
+
+ if (window.confirm("Tu vas voir ce site est top !")){ 
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "Nouvelle fenêtre", "");
 }
+}
+
+/*window.setTimeout(myFunction, 2000);
+
+function myFunction() {
+window.confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp !!!");
+
+ if (window.confirm("Une nouvelle fenêtre va s'ouvrir.")) { 
+    window.open("http://test.com/fenetre.html", "Nouvelle fenêtre", "");
+}
+*/
